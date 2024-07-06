5fa70729135893d02cc3a6e379777aa0fff6df4b
muhammad haseeb <muhammad.haseeb@aeratechnology.com>
2024-07-06 21:08:27 +0200

    Fix files naming

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
+import './index.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './App'
 import reportWebVitals from './reportWebVitals'
+import Routes from './Routes'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
